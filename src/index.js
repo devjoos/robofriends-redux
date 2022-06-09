b7f3de109819d913d35592adb72f57038f42523b
Sam Joos <samjoos@Sams-MacBook-Pro.local>
2022-06-09 16:51:24 -0600

    updating to newest service worker

--- src/index.js
+++ src/index.js
@@ -8,21 +8,24 @@ import 'tachyons';
 
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
 
-const logger = createLogger() 
+const logger = createLogger()
 
-const rootReducers = combineReducers({requestRobots, searchRobots})
+const rootReducers = combineReducers({ requestRobots, searchRobots })
 
 const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
